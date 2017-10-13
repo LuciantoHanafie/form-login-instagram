110c6220e59d87c4616ba9d68b3873d87b2caedc
LuciantoHanafie <30461135+LuciantoHanafie@users.noreply.github.com>
2017-10-13 11:23:46 +0700

    Add files via upload

---
+++ hasil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<table>
+	<tr><td>Email :</td><td><?php echo $_GET ["Email"]; ?></td></tr><br>
+	<tr><td>Full Name :</td><td><?php echo $_GET ["FullName"]; ?></td></tr><br>
+	<tr><td>User Name :</td><td><?php echo $_GET ["UserName"]; ?></td></tr><br>
+	<tr><td>Password :</td><td><?php echo $_GET ["Pass"]; ?></td></tr><br>
+</table>
+</body>
+</html>
